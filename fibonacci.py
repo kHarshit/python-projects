e2ac20927b405371465af32f8a4ae43ae3d6c17a
Harshit Kumar <harshitk.997@gmail.com>
2017-02-25 21:39:04 +0530

    Add fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,51 @@
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n-1):
+        # print(a, end=' ')
+        a, b = b, a+b
+    return a
+
+
+# def fib_generator(n):
+#     a, b = 1, 1
+#     while True:
+#         yield a
+#         a, b = b, a+b
+
+# f = fib_generator()
+# for i in range(6):
+#     print(f.next())
+
+
+
+numFibCalls_rec = 0
+def fibonacci_rec(n):
+    """This algorithm is inefficient because it recalculates the same value many times"""
+    global numFibCalls_rec
+    numFibCalls_rec += 1
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci_rec(n-1) + fibonacci_rec(n-2)
+
+
+numFibCalls_memoize = 0
+def fib_memoization(n, dict):
+    """It is an efficient algorithm as it first lookup if value is already calculated"""
+    global numFibCalls_memoize
+    numFibCalls_memoize += 1
+    if n in dict:
+        return dict[n]
+    else:
+        ans = fib_memoization(n - 1, dict) + fib_memoization(n - 2, dict)
+        dict[n] = ans
+        return ans
+
+d = {1:1, 2:1}
+
+
+
+print("fibonacci num:", fibonacci(16))
+# print("fibonacci num:", fib_generator(16), ", numFibCalls:", )
+print("fibonacci num:", fibonacci_rec(16), ", numFibCalls_rec:", numFibCalls_rec)
+print("fibonacci num:", fib_memoization(16, d), ", numFibCalls_memoize:", numFibCalls_memoize)
