e08a7af5306ce30fb6aba67107b0068a44fdc4af
Harshit Kumar <harshitk.997@gmail.com>
2017-02-26 20:21:45 +0530

    Add pythagoras.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # python-projects
+A craft cannot be learned by reading alone; practice is also necessary.
+
+
+The project involves following problems:
+IN production
---
+++ pythagoras.py
@@ -0,0 +1,36 @@
+def pythagorean_triplets(n):
+    """Note that this algorithm is not efficient"""
+    for x in range(n):
+        for y in range(x+1, n):
+            for z in range(y+1, n):
+                if x*x + y*y == z*z:
+                    print(x, y, z)
+
+def pythagorean_triplets_eff(n):
+    for x in range(n):
+        for y in range(x+1, n):
+            z = y + 1
+            while z*z < x*x + y*y:
+                z += 1
+            if z*z == x*x + y*y:
+                print(x, y, z)
+
+def pythagorean_triplets_efficient(n):
+    """It is an efficient algorithm to fing pythagorean triplets"""
+    for x in range(1, n):
+        y = x + 1
+        z = y + 1
+        while z <= n:
+            while z*z < x*x + y*y:
+                z += 1
+            if z*z == x*x + y*y:
+                print(x, y, z)
+            y += 1
+
+
+
+pythagorean_triplets(30)
+print()
+pythagorean_triplets_eff(30)
+print()
+pythagorean_triplets_efficient(30)
