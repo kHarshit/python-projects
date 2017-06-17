e8c02bdd6bbb63ade5b3386c4213dfd015980b0d
Harshit Kumar <kumar_harshit@outlook.com>
2017-06-17 14:52:59 +0530

    Implement bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,14 +1,31 @@
+"""pseudo-code"""
+
+"""
+BUBBLE-SORT(A):  // A[1....A.length]
+for i=1 to A.length - 1
+    for j=A.length downto i + 1
+        if A[j] < A[j-1]
+            exchange A[j] with A[j-1]
+"""
+
+
+# def bubble_sort(ls):
+#     """The complexity of this algorithm is O(n^2)."""
+#     flag = False
+#     while not flag:
+#         flag = True
+#         for i in range(1, len(ls)):
+#             if ls[i-1] > ls[i]:
+#                 ls[i], ls[i-1] = ls[i-1], ls[i]
+#                 flag = False
+
 def bubble_sort(ls):
     """The complexity of this algorithm is O(n^2)."""
-    flag = False
-    while not flag:
-        flag = True
-        for i in range(1, len(ls)):
-            if ls[i-1] > ls[i]:
-                temp = ls[i]
-                ls[i] = ls[i-1]
-                ls[i-1] = temp
-                flag = False
+    for i in range(len(ls)-2):
+        for j in range(len(ls)-1, i, -1):
+            if ls[j-1] > ls[j]:
+                ls[j], ls[j-1] = ls[j-1], ls[j]
+
 
 list_in = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 print(list_in)
