2e736c2a050ade871b24b85f802eadce0adbcf47
Harshit Kumar <kumar_harshit@outlook.com>
2017-06-18 20:30:06 +0530

    Add randomized quick sort

--- qsort.py
+++ qsort.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 """pseudo code"""
 
 """
@@ -42,7 +45,58 @@ def qsort(A, l, r):
 def sort(A):
     qsort(A, 0, len(A)-1)
 
+print("QUICKSORT:")
 list_in = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-print(list_in)
+print("I:", list_in)
 sort(list_in)
-print(list_in)
+print("O:", list_in)
+
+
+##############################################
+"""
+RANDOMIZED-PARTITION(A, l, r):
+p = RANDOM(l, r)
+i = l + 1
+for j=l+1 to r
+    if A[j] < p  // if A[j] > p, do nothing
+        swap A[j] and A[i]
+        i = i + 1
+swap A[l] and A[i-1]
+return
+
+RANDOMIZED-QUICKSORT(A, l, r):
+if l < r
+    p = RANDOMIZED-PARTITION(A, l, r)
+    RANDOMIZED-QUICKSORT(A, l, p-1)
+    RANDOMIZED-QUICKSORT(A, p+1, r)
+
+"""
+
+
+def random_partition(A, l, r):
+    p = A[randint(l, r)]
+    i = l + 1
+    for j in range(i, r):
+        if A[j] <= p:
+            A[j], A[i] = A[i], A[j]
+            i += 1
+    A[l], A[i-1] = A[i-1], A[l]
+    return i
+
+
+def random_qsort(A, l, r):
+    """uses quick-sort to return a sorted array"""
+    if l < r:
+        q = random_partition(A, l, r)
+        random_qsort(A, l, q-1)
+        random_qsort(A, q+1, r)
+
+
+def random_sort(A):
+    random_qsort(A, 0, len(A)-1)
+
+print("RANDOMIZED-QUIKSORT:")
+list_in_rand = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+print("I:", list_in_rand)
+random_sort(list_in_rand)
+print("O:", list_in_rand)
